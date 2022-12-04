23e802f510ec47bcac41ae06aa1c7631052bef83
Andres Flores <andres06037@gmail.com>
2022-12-04 17:17:18 -0600

    Unfinished part 2, WIP

---
+++ day4/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+rstRight >= secLeft:
+		# 	totalOverlap += 1
+		# elif secLeft <= firstLeft and secRight <= firstRight:
+		# 	totalOverlap += 1
+		# elif firstLeft <= secLeft and firstRight >= secRight:
+		# 	totalOverlap += 1
+		# elif secLeft <= firstLeft and secRight >= firstRight:
+		# 	totalOverlap += 1
