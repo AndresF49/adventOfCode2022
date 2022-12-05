e98cb9920175e2f281f9a1467b7ba79d141a51e0
Andres F <33458180+AndresF49@users.noreply.github.com>
2022-12-05 13:21:10 -0600

    Unnecessary

--- day4/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-rstRight >= secLeft:
-		# 	totalOverlap += 1
-		# elif secLeft <= firstLeft and secRight <= firstRight:
-		# 	totalOverlap += 1
-		# elif firstLeft <= secLeft and firstRight >= secRight:
-		# 	totalOverlap += 1
-		# elif secLeft <= firstLeft and secRight >= firstRight:
-		# 	totalOverlap += 1
