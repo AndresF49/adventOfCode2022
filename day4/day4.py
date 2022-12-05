0367078998de7207f001f1aa999405a10b68687b
Andres F <33458180+AndresF49@users.noreply.github.com>
2022-12-05 13:20:53 -0600

    Finished part 2

--- day4/day4.py
+++ day4/day4.py
@@ -8,17 +8,28 @@ def main():
 		# print(chore)
 		firstPairLeft = chore[0][0:chore[0].find('-')]
 		firstPairRight = chore[0][chore[0].find('-')+1:]
-		secPairLeft = chore[1][0:chore[0].find('-')]
-		secPairRight = chore[1][chore[0].find('-')+1:]
+		secPairLeft = chore[1][0:chore[1].find('-')]
+		secPairRight = chore[1][chore[1].find('-')+1:]
+
+		# print(f"{firstPairLeft} {firstPairRight} | {secPairLeft} {secPairRight}")
+
+		s1 = set(range(int(firstPairLeft),int(firstPairRight)+1))
+		s2 = set(range(int(secPairLeft),int(secPairRight)+1))
+
+		if s1.intersection(s2):
+			# print(f"{firstPairLeft} {firstPairRight} | {secPairLeft} {secPairRight}")
+			totalFullyContained += 1
+# 		else:
+# 			print(f"{firstPairLeft} {firstPairRight} | {secPairLeft} {secPairRight}")
 		# print(f"{firstPairLeft} {firstPairRight} {secPairLeft} {secPairRight}")
 
-		if firstPairLeft <= secPairLeft and firstPairRight >= secPairRight:
-			totalFullyContained += 1
-		elif secPairLeft <= firstPairLeft and secPairRight >= firstPairRight:
-			totalFullyContained += 1
+		# if firstPairLeft <= secPairLeft and firstPairRight >= secPairRight:
+		# 	totalFullyContained += 1
+		# elif secPairLeft <= firstPairLeft and secPairRight >= firstPairRight:
+		# 	totalFullyContained += 1
 	
 	print(totalFullyContained)
-	# 841 is too high
+	# 841 is too low
 
 		
 if __name__ == "__main__":
